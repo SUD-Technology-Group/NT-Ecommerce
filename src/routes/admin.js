e2ce8dc78d460d4c1286b35485bc875e49f5cb61
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2022-10-30 19:51:04 +0700

    setup frontend

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-var admin = 'admin/pages/';
+var baseURL = 'admin/pages/';
 // [/admin/]
 router.get('/', function (req, res, next) {
-  res.render(admin + 'home/index', { title: 'NT E-commerce', layout: 'admin' });
+    res.render(baseURL + 'home/index', { title: 'NT E-commerce', layout: 'admin' });
 });
 
 // [/admin/dich-vu]
 router.get('/dich-vu', function (req, res, next) {
-  res.render('admin/service/index', { title: 'NT E-commerce', layout: 'admin' });
+    res.render(baseURL + 'service/index', { title: 'NT E-commerce', layout: 'admin' });
 });
 
 module.exports = router;
--- src/routes/client.js
+++ src/routes/client.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+var baseURL = 'client/pages/';
 // [/]
 router.get('/', function (req, res, next) {
-  res.render('client/pages/home/index', {
-    layout: 'main',
-    title: 'NT E-commerce',
-  });
+    res.render(baseURL + 'home/index', {
+        layout: 'main',
+        title: 'NT E-commerce',
+    });
 });
 
 // [/dich-vu]
 router.get('/dich-vu', function (req, res, next) {
-  res.render('client/service/index', { title: 'NT E-commerce' });
+    res.render(baseURL + 'service/index', { title: 'NT E-commerce' });
 });
 
 module.exports = router;
