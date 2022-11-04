eb897cde3048d827a2e60cf20acf1c27a40705b1
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2022-11-04 21:05:45 +0700

    Project complete, merge Duong, Minh

--- src/data/mock.js
+++ src/data/mock.js
@@ -90,25 +90,29 @@ const introduce = [
 ];
 
 const albumList = [
-    { image: 'assets/images/VINACONEX-7.jpg', title: 'Vinaconex-7 (BGI Group)', subtitle: 'Nhận diện Website' },
-    { image: 'assets/images/vina-logistics.png', title: 'Vina Logistics', subtitle: 'Nhận diện' },
-    { image: 'assets/images/vietstarland.jpg', title: 'Vietstarland', subtitle: 'Nhận diện' },
-    { image: 'assets/images/videc.jpg', title: 'Videc Group', subtitle: 'Nhận diện, sản phẩm số, website' },
-    { image: 'assets/images/ucbi.jpg', title: 'UCBI', subtitle: 'Nhận diện Website' },
-    { image: 'assets/images/tv-construction.jpg', title: 'TV Construction', subtitle: 'Nhận diện' },
-    { image: 'assets/images/the-youwin-coffee.jpg', title: 'The Youwin Coffeee', subtitle: 'Khác, Nhận diện, Website' },
-    { image: 'assets/images/seurice.jpg', title: 'Seu Rice', subtitle: 'Bao bì, Nhận diện' },
-    { image: 'assets/images/rimac.png', title: 'Rimacx', subtitle: 'Nhận diện' },
-    { image: 'assets/images/qp.jpg', title: 'Quang Phúc Group', subtitle: 'Nhận diện, Website' },
+    { image: '/assets/images/VINACONEX-7.jpg', title: 'Vinaconex-7 (BGI Group)', subtitle: 'Nhận diện Website' },
+    { image: '/assets/images/vina-logistics.png', title: 'Vina Logistics', subtitle: 'Nhận diện' },
+    { image: '/assets/images/vietstarland.jpg', title: 'Vietstarland', subtitle: 'Nhận diện' },
+    { image: '/assets/images/videc.jpg', title: 'Videc Group', subtitle: 'Nhận diện, sản phẩm số, website' },
+    { image: '/assets/images/ucbi.jpg', title: 'UCBI', subtitle: 'Nhận diện Website' },
+    { image: '/assets/images/tv-construction.jpg', title: 'TV Construction', subtitle: 'Nhận diện' },
+    {
+        image: '/assets/images/the-youwin-coffee.jpg',
+        title: 'The Youwin Coffeee',
+        subtitle: 'Khác, Nhận diện, Website',
+    },
+    { image: '/assets/images/seurice.jpg', title: 'Seu Rice', subtitle: 'Bao bì, Nhận diện' },
+    { image: '/assets/images/rimac.png', title: 'Rimacx', subtitle: 'Nhận diện' },
+    { image: '/assets/images/qp.jpg', title: 'Quang Phúc Group', subtitle: 'Nhận diện, Website' },
 ];
 
 const sectionList = [
-    { icon: 'assets/images/icon_cat3.png', size: '28', name: 'Nhận diện' },
-    { icon: 'assets/images/icon_cat5.png', size: '23', name: 'Sản phẩm số' },
-    { icon: 'assets/images/icon_cat2.png', size: '28', name: 'Bao bì' },
-    { icon: 'assets/images/icon_cat6.png', size: '23', name: 'In ấn' },
-    { icon: 'assets/images/icon_cat5.png', size: '28', name: 'Website' },
-    { icon: 'assets/images/icon_cat1.png', size: '23', name: 'Khác' },
+    { icon: '/assets/images/icon_cat3.png', size: '23', name: 'Nhận diện' },
+    { icon: '/assets/images/icon_cat5.png', size: '23', name: 'Sản phẩm số' },
+    { icon: '/assets/images/icon_cat2.png', size: '23', name: 'Bao bì' },
+    { icon: '/assets/images/icon_cat6.png', size: '23', name: 'In ấn' },
+    { icon: '/assets/images/icon_cat5.png', size: '23', name: 'Website' },
+    { icon: '/assets/images/icon_cat1.png', size: '23', name: 'Khác' },
 ];
 
 module.exports = {
