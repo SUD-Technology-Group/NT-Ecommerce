f39c9d2165019d150f8e2238b4ca35421114d0fa
tanphat1801 <tanphat200265@gmail.com>
2022-11-15 13:10:17 +0700

    denied routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const clientRouter = require('./client');
 const adminRouter = require('./admin');
 
+router.get('*', function (req, res, next) {
+    res.render('client/pages/404', { title: 'NT E-commerce', layout: '' });
+});
+
 // [/]
 router.use('/', clientRouter);
 
